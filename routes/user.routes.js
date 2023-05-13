92b65ecdfb563a4ec7eaf95b9caeecec623f9fce
Fran Linde Blázquez <franlindebl@gmail.com>
2023-05-13 19:42:00 +0200

    Added login request without any security

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -117,4 +117,37 @@ router.put("/:id", async (req, res, next) => {
   }
 });
 
+// LOGIN DE USUARIOS
+router.post("/login", async (req, res, next) => {
+  try {
+    // const email = req.body.email;
+    // const password = req.body.password;
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Se deben especificar los campos email y password" });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      // return res.status(404).json({ error: "No existe un usuario con ese email" });
+      // Por seguridad mejor no indicar qué usuarios no existen
+      return res.status(401).json({ error: "Email y/o contraseña incorrectos" });
+    }
+
+    // Comprueba la pass
+    if (user.password === password) {
+      // Quitamos password de la respuesta
+      const userWithoutPass = user.toObject();
+      delete userWithoutPass.password;
+
+      return res.status(200).json(userWithoutPass);
+    } else {
+      return res.status(401).json({ error: "Email y/o contraseña incorrectos" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = { userRouter: router };
